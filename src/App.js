b0513c25fb57e4d7c1666fe22b4490775f623103
chetanbaruzo <chetan.nada@baruzotech.com>
2021-10-04 13:35:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+      <div className="App border-2 rounded-lg bg-blue-500 text-7xl text-white text-center h-full">
+      <h1 className="pt-24">Thanks for warm welcome 🙏</h1>
+      <br />
+      <a href="https://www.baruzotech.com/" target="_blank"><img className="mx-auto border-2 rounded-xl object-contain md:object-scale-down mb-24" src="/Baruzotech.jfif" /></a>
+      </div>
+  );
+}
+
+export default App;
