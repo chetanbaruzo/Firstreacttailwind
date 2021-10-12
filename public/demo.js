6f8067a59c5e391ebe1a4e463cc6a3423e35c72b
chetanbaruzo <chetan.nada@baruzotech.com>
2021-10-12 13:18:44 +0530

    learning JSON

--- public/demo.js
+++ public/demo.js
@@ -1,3 +1,72 @@
+let object1 = {
+  id: 7,
+  name: "chetan",
+  Hobbie: "Watching Movie",
+  languageUsing: "React",
+  country: "India",
+  male: true,
+  book: [
+    {
+      language: "React",
+      edition: "First",
+    },
+  ],
+};
+
+
+let { id, name, book } = object1;
+document.write("<br>");
+document.write("id = " + id);
+document.write("<br>");
+document.write("name = " + name);
+document.write("<br>");
+document.write("Hobbie = " + object1.Hobbie);
+document.write("<br>");
+document.write("Language = " + object1.languageUsing);
+document.write("<br>");
+document.write("Country = " + object1.country);
+document.write("<br>");
+document.write("Male = " + object1.male);
+document.write("<br>");
+
+document.write("Book Language= " + book[0].language);
+document.write("<br>");
+
+
+
+let str = "chetan";
+let copystr = str;
+str = "hello"
+console.log(str); //hello
+console.log(copystr); //chetan
+
+let arr = [1,2,3,4,5];
+let copyarr = arr;
+arr[5] = 6;
+console.log(arr); //[1, 2, 3, 4, 5, 6]
+console.log(copyarr); //[1, 2, 3, 4, 5, 6]
+
+
+var emptyobj = {};
+console.log(emptyobj);
+var newobj = new Object();
+console.log(newobj);
+newobj = {"name": "chetan"};
+newobj = {"gender": "male"};
+console.log(newobj);
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // Default Binding in JavaScript
 // // this keyword is refered to as global object and name() is a standalone and unattached function, so it is bound to the global scope.
 // function alert() {
@@ -5,7 +74,6 @@
 // }
 // var company = "BaruzoTech";
 // alert(); //BaruzoTech is great.
-
 
 // // Implicit Binding in JavaScript
 // //when you call a function using dot notation, this is implicitly bound to the object the function is being called from.
@@ -20,9 +88,8 @@
 // }
 // mymobile.alert2(); // apple is an awesome mobile
 
-
 // // Explicit binding in JavaScript
-// // if we want to force a function to use an object as its context without putting a property function reference on the object, 
+// // if we want to force a function to use an object as its context without putting a property function reference on the object,
 // // We have two utility methods to achieve this: call() and apply().
 // function alert3(){
 //     console.log(this.name + ' is calling.')
